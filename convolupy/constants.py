403c807d6279e539a52d4857968c251324336726
David Warde-Farley <dwf@cs.toronto.edu>
2009-11-23 04:45:33 -0500

    Use __future__ instead of 2. / 3. in constants.py.

--- convolupy/constants.py
+++ convolupy/constants.py
@@ -3,8 +3,10 @@ Useful numerical constants. Right now just a home for the inner
 and outer tanh scaling factors.
 """
 
+from __future__ import division
+
 # Chosen so that the sigmoid has fixed points at -1 and +1, which are also
 # the points of maximum second derivative. This aids in fast convergence. 
 TANH_OUTER = 1.7159
-TANH_INNER = 2. / 3.
+TANH_INNER = 2 / 3
 
